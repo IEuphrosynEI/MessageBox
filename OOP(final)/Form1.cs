5332907d098b58df0f99f1054db9215c33129ada
unknown <seansain16@gmail.com>
2022-04-29 18:40:47 +0800

    FirstCommit

---
+++ OOP(final)/Form1.cs
@@ -0,0 +1,15 @@
+namespace OOP_final_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sean Kieran Gloraini Sain", "EuphrosynE", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
